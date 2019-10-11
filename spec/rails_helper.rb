86f6a77e0afc6b0bba5631ecb81dc612f74d9a01
ninjadev999 <ninjadev999@gmail.com>
2019-10-11 02:35:21 -0700

    added auth user and jwt token

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,6 +44,8 @@ end
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 

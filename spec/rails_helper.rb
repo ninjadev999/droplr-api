a90f2a22a7575c64332b2b3b95bfc46c157fa87c
ninjadev999 <ninjadev999@gmail.com>
2019-10-10 22:04:29 -0700

    added rest api for each models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,8 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -85,5 +87,7 @@ RSpec.configure do |config|
       example.run
     end
   end
-  
+
+  config.include RequestSpecHelper, type: :request
+
 end
